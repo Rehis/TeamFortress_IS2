2524187db5b0fd5818e1f995c3747c91a12a7b5f
RMGonzalo <60696655+RMGonzalo@users.noreply.github.com>
2021-05-20 00:29:30 +0200

    Update ArticuloRepository.java

--- src/main/java/com/tienda/api/ApiRest/repository/ArticuloRepository.java
+++ src/main/java/com/tienda/api/ApiRest/repository/ArticuloRepository.java
@@ -7,9 +7,10 @@ import org.springframework.data.jpa.repository.Query;
 import com.tienda.api.ApiRest.model.Articulo;
 
 public interface ArticuloRepository extends JpaRepository<Articulo, Integer>{
+	
 
-	@Query("from Articulo a order by a.nombre")
-	List<Articulo> buscarTodos();
+    @Query("from Articulo a order by a.nombre")
+    List<Articulo> buscarTodos();
     
     @Query("from Articulo a where a.nombre like %?1%")
     List<Articulo> buscar(String nombre);
@@ -22,6 +23,7 @@ public interface ArticuloRepository extends JpaRepository<Articulo, Integer>{
 	
     @Query("from Articulo a where a.categoria like %?1%")
     List<Articulo> buscarCat(String categorias);
+	
     
     /*Esta funcion hace lo mismo que buscar pero es generada por JPA automaticamente
     Esto es debido a que por el nombre findBy deduce que es busqueda con Nombre busca el parametro
